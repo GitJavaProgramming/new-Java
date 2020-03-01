65f6b253634034a2a0e0f344f888836b65489ebc
GitJavaProgramming <455547940@qq.com>
2020-03-01 21:04:16 +0800

    apache commons io 工具类源码阅读与应用

---
+++ src/main/java/org/pp/apache/common/io/testIO.java
@@ -0,0 +1,28 @@
+package org.pp.apache.common.io;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.file.FileSystem;
+
+/**
+ * 参考
+ * http://codingdict.com/article/8775
+ */
+public class testIO {
+
+    @Test
+    public void test(){
+        System.out.println(FileUtils.getTempDirectoryPath());
+        System.out.println(FileUtils.getUserDirectoryPath());
+//        FileUtils.copyFile();
+//        FileUtils.write();
+//        FileUtils.readFileToByteArray()
+//        FileUtils.readFileToString()
+
+//        IOUtils.copy()
+    }
+}
