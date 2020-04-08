a8dafa6019819ea0181a8af986bdafbd58415cbf
GitJavaProgramming <455547940@qq.com>
2020-04-09 07:30:42 +0800

    项目整理--基础 算法与数据结构 java并发 设计模式 jvm 函数式编程

--- src/main/java/org/pp/socket/aio/AsyncTimeClient.java
+++
@@ -1,7 +0,0 @@
-package org.pp.socket.aio;
-
-public class AsyncTimeClient {
-    public static void main(String[] args) {
-        new Thread(new AsyncTimeClientHandler("127.0.0.1",8000), "AIO-asyncTimeClientHandler-001").start();
-    }
-}
--- src/main/java/org/pp/socket/aio/AsyncTimeServer.java
+++
@@ -1,8 +0,0 @@
-package org.pp.socket.aio;
-
-public class AsyncTimeServer {
-    
-    public static void main(String[] args) {
-        new Thread(new AsyncTimeServerHandler(8000), "AIO-asyncTimeServerHandler-001").start();
-    }
-}
