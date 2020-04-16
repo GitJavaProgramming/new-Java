832743f50cc80df24383b3dc3282059bf7aa3bc3
GitJavaProgramming <455547940@qq.com>
2020-04-16 09:34:49 +0800

    图ADT--深度优先遍历与广度优先遍历Java实现

---
+++ src/main/java/org/pp/java8/algorithm/datastruct/nonlinear/AbstractGraph.java
@@ -0,0 +1,78 @@
+package org.pp.java8.algorithm.datastruct.nonlinear;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public abstract class AbstractGraph implements Graph {
+
+    @Override
+    public void dfs(Graph g) {
+        // 初始化图
+        boolean[] visited = initGraph(g);
+        // 深度优先遍历图
+        int vertices = g.vertices();
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i]) {
+                dfs(g, i, visited);
+            }
+        }
+    }
+
+    private void dfs(Graph g, int v, boolean[] visited) {
+        preVisit(g, v);
+        visited[v] = true; // 设置已访问
+        for (int w = g.first(v); w < g.vertices(); w = g.next(v, w)) { // 深度遍历
+            if (!visited[w]) { // 可达路径上，没访问过的就访问
+                dfs(g, w, visited);
+            }
+        }
+        postVisit(g, v);
+    }
+
+    /**************************************************************************************/
+    @Override
+    public void bfs(Graph g) {
+        boolean[] visited = initGraph(g);
+        // 广度优先遍历图
+        int vertices = g.vertices();
+        Queue<Integer> queue = new LinkedBlockingQueue(); // 使用队列保存邻接点
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i]) {
+                bfs(g, i, visited, queue);
+            }
+        }
+    }
+
+    private void bfs(Graph g, int start, boolean[] visited, Queue<Integer> queue) {
+        queue.offer(start);
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            Integer v = queue.poll();
+            preVisit(g, v);
+            for (int w = g.first(v); w < g.vertices(); w = g.next(v, w)) {
+                if (!visited[w]) {
+                    visited[w] = true;
+                    queue.offer(w);
+                }
+            }
+            postVisit(g, v);
+        }
+    }
+
+    /*******************************************业务方法*******************************************/
+    /**
+     * 初始化图，未遍历初始状态
+     */
+    protected boolean[] initGraph(Graph g) {
+        int vertices = g.vertices();
+        boolean[] visited = new boolean[vertices];
+        for (int i = 0; i < vertices; i++) {
+            visited[i] = false;
+        }
+        return visited;
+    }
+
+    protected abstract void preVisit(Graph g, int v);
+
+    protected abstract void postVisit(Graph g, int v);
+}
---
+++ src/main/java/org/pp/java8/algorithm/datastruct/nonlinear/Graph.java
@@ -0,0 +1,46 @@
+package org.pp.java8.algorithm.datastruct.nonlinear;
+
+/**
+ * 图：顶点集合和边的集合的二元组，要表示一个图G=(V, E),有两种标准的方法：邻接表和邻接矩阵
+ */
+public interface Graph {
+    /**
+     * 图中顶点数
+     */
+    int vertices();
+
+    /**
+     * 图中的边数
+     */
+    int edges();
+
+    /**
+     * 返回与顶点关联的第一条边
+     */
+    int first(int v);
+
+    /**
+     * 返回下一条与此顶点关联的边
+     */
+    int next(int v1, int v2);
+
+    /**
+     * 设置一条边的权，权重>0
+     */
+    void setEdge(int v1, int v2, int wgt);
+
+    /**
+     * 获得边的权重,使用起点和终点表示一条边，如果边不存在则权重为0
+     */
+    int weight(int v1, int v2);
+
+    /**
+     * 深度优先遍历,沿着某一分支搜索至末端，然后回溯，沿着另一分支搜索
+     */
+    void dfs(Graph g);
+
+    /**
+     * 广度优先遍历，在进一步深入访问其他顶点前，检查所有的邻接点
+     */
+    void bfs(Graph g);
+}
